26bdf0035862f772b8411015f734a6bb69ffb5a7
Leon Merten Lohse <leon@judicator.user.kellnerweg.de>
2016-12-18 12:16:00 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+  name = 'bottle-authenticate',
+  version = '0.2',
+  description = 'Authentication for Bottle',
+  author = 'Leon Merten Lohse',
+  author_email = 'leon@green-side.de',
+  url = 'https://www.github.com/llohse/bottle-authenticate',
+  license = 'MIT',
+  platforms = 'any',
+  py_modules = [ 'bottle_authenticate', ],
+  install_requires = ['bottle>=0.9',],
+  classifiers = [
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+  )
